951b3c462107906c7bcb4b8a1aa3de480af39f59
wscjxky <619677632@qq.com>
2018-10-02 23:19:26 +0800

    o

--- 第五章/task1.py
+++ 第五章/task1.py
@@ -1,7 +1,23 @@
-import numpy as np
+import requests
+import threading
 
-def a(b):
-    b-=1
-b=3
-a(3)
-print(b)
+
+def f():
+    req=requests.post('http://567cg.cn/Default/Update',
+                  {'phonecode': 123123,
+    'action': '提交短信验证码'})
+    print(req.text)
+    req = requests.post('http://567cg.cn/Default/Update',
+                        {
+                         'action': '获取验证码'})
+    print(req.text)
+
+f()
+threadlist = []
+for i in range(10):
+    threadlist.append(threading.Thread(target=f))
+for t in threadlist:
+    t.setDaemon(True)  # 如果你在for循环里用，不行， 因为上一个多线程还没结束又开始下一个
+    t.start()
+for j in threadlist:
+    j.join()
